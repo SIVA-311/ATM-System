886775ce4e760d1c297098ae185c0f4f46e46935
Siva <sivaamart7@gmail.com>
2026-03-09 13:43:15 +0530

    first commit

---
+++ addi.java
@@ -0,0 +1,12 @@
+public class addi {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = a;
+
+        a = 10;
+
+        System.out.println(a); // 10
+        System.out.println(b); // 5
+    }
+}
